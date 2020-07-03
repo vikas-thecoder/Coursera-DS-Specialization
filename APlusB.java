93ffe493a0192c598767dd5c679e9e5a467243cb
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-07-03 22:12:42 +0530

    Update APlusB.java

--- APlusB.java
+++ APlusB.java
@@ -5,9 +5,7 @@ Compute the sum of two single digit numbers.
 Input: Two single digit numbers.
 Output: The sum of these numbers.
 2 + 3 = 5
-We start from this ridiculously simple problem to show you the
-pipeline of reading the problem statement, designing an algorithm, implementing it, testing and debugging your program, and submitting it to
-the grading system.
+We start from this ridiculously simple problem to show you the pipeline of reading the problem statement, designing an algorithm, implementing it, testing and debugging your program, and submitting it to the grading system.
 Input format. Integers a and b on the same line (separated by a space).
 Output format. The sum of a and b.
 Constraints. 0 ≤ a,b ≤ 9.
