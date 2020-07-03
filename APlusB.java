572df340f1de81be63ddcc5770c7d9335296e61e
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-07-03 22:06:32 +0530

    Add files via upload
    Sum of Two Digits Problem

---
+++ APlusB.java
@@ -0,0 +1,36 @@
+/*
+
+                                                       Sum of Two Digits Problem
+Compute the sum of two single digit numbers.
+Input: Two single digit numbers.
+Output: The sum of these numbers.
+2 + 3 = 5
+We start from this ridiculously simple problem to show you the
+pipeline of reading the problem statement, designing an algorithm, implementing it, testing and debugging your program, and submitting it to
+the grading system.
+Input format. Integers a and b on the same line (separated by a space).
+Output format. The sum of a and b.
+Constraints. 0 ≤ a,b ≤ 9.
+Sample.
+Input:
+9 7
+Output:
+16
+
+*/
+
+
+import java.util.Scanner;
+
+class APlusB {
+    static int sumOfTwoDigits(int first_digit, int second_digit) {
+        return first_digit + second_digit;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(sumOfTwoDigits(a, b));
+    }
+}
