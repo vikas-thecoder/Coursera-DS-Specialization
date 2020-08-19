81cbf23c67f56de56637c52da18f333618040a04
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-19 21:16:01 +0530

    Update FibonacciHuge.java

--- Week 2/FibonacciHuge.java
+++ Week 2/FibonacciHuge.java
@@ -1,3 +1,24 @@
+/*
+Problem Description
+Task. Given two integers 𝑛 and 𝑚, output 𝐹𝑛 mod 𝑚 (that is, the remainder of 𝐹𝑛 when divided by 𝑚).
+Input Format. The input consists of two integers 𝑛 and 𝑚 given on the same line (separated by a space).
+Constraints. 1 ≤ 𝑛 ≤ 1014
+, 2 ≤ 𝑚 ≤ 103.
+Output Format. Output 𝐹𝑛 mod 𝑚.
+Sample 1.
+Input:
+239 1000
+Output:
+161
+𝐹239 mod 1 000 = 39 679 027 332 006 820 581 608 740 953 902 289 877 834 488 152 161 (mod 1 000) = 161.
+
+Sample 2.
+Input:
+2816213588 239
+Output:
+151
+𝐹2 816 213 588 does not fit into one page of this file, but 𝐹2 816 213 588 mod 239 = 151.
+*/
 import java.util.*;
 
 public class FibonacciHuge {
