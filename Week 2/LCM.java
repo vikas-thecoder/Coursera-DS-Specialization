e20002fe938d924709091dedf4bc326c22327b21
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-19 21:17:15 +0530

    Update LCM.java

--- Week 2/LCM.java
+++ Week 2/LCM.java
@@ -1,3 +1,23 @@
+/*
+Problem Description
+Task. Given two integers 𝑎 and 𝑏, find their least common multiple.
+Input Format. The two integers 𝑎 and 𝑏 are given in the same line separated by space.
+Constraints. 1 ≤ 𝑎, 𝑏 ≤ 107.
+Output Format. Output the least common multiple of 𝑎 and 𝑏.
+Sample 1.
+Input:
+6 8
+Output:
+24
+Among all the positive integers that are divisible by both 6 and 8 (e.g., 48, 480, 24), 24 is the smallest
+one.
+
+Sample 2.
+Input:
+761457 614573
+Output:
+467970912861
+*/
 import java.util.*;
 
 public class LCM {
