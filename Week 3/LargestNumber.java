0b4af76404204ddf058705b00e0a7a406cde001c
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-21 19:34:16 +0530

    Update LargestNumber.java

--- Week 3/LargestNumber.java
+++ Week 3/LargestNumber.java
@@ -1,3 +1,36 @@
+/*
+Problem Description
+Task. Compose the largest number out of a set of integers.
+Input Format. The first line of the input contains an integer 𝑛. The second line contains integers 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+Constraints. 1 ≤ 𝑛 ≤ 100; 1 ≤ 𝑎𝑖 ≤ 103 for all 1 ≤ 𝑖 ≤ 𝑛.
+
+Output Format. Output the largest number that can be composed out of 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+
+Sample 1.
+Input:
+2
+21 2
+Output:
+221
+Note that in this case the above algorithm also returns an incorrect answer 212.
+
+Sample 2.
+Input:
+5
+9 4 6 1 9
+Output:
+99641
+In this case, the input consists of single-digit numbers only, so the algorithm above computes the right answer.
+
+Sample 3.
+Input:
+3
+23 39 92
+Output:
+923923
+As a coincidence, for this input the above algorithm produces the right result, though the input does
+not have any single-digit numbers
+*/
 import java.util.*;
 
 public class LargestNumber {
