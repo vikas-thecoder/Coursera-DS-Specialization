36edb9c59a30dc1a40d73da295e6f01fa339f9d8
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-21 19:32:13 +0530

    Update DifferentSummands.java

--- Week 3/DifferentSummands.java
+++ Week 3/DifferentSummands.java
@@ -1,3 +1,37 @@
+/*
+Problem Description
+Task. The goal of this problem is to represent a given positive integer 𝑛 as a sum of as many pairwise
+distinct positive integers as possible. That is, to find the maximum 𝑘 such that 𝑛 can be written as
+𝑎1 + 𝑎2 + · · · + 𝑎𝑘 where 𝑎1, . . . , 𝑎𝑘 are positive integers and 𝑎𝑖 = 𝑎𝑗 for all 1 ≤ 𝑖 < 𝑗 ≤ 𝑘.
+
+Input Format. The input consists of a single integer 𝑛.
+
+Constraints. 1 ≤ 𝑛 ≤ 109.
+
+Output Format. In the first line, output the maximum number 𝑘 such that 𝑛 can be represented as a sum
+of 𝑘 pairwise distinct positive integers. In the second line, output 𝑘 pairwise distinct positive integers
+that sum up to 𝑛 (if there are many such representations, output any of them).
+
+Sample 1.
+Input:
+6
+Output:
+3
+1 2 3
+
+Sample 2.
+Input:
+8
+Output:
+3
+1 2 5
+
+Sample 3.
+Input:
+2
+Output:
+12
+*/
 import java.util.*;
 
 public class DifferentSummands {
