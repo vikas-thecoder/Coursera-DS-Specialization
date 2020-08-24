1f3733d92832722786fd7fa9bb88b8fc84ea4a04
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-24 20:36:06 +0530

    Update Sorting.java

--- Week 4/Sorting.java
+++ Week 4/Sorting.java
@@ -1,3 +1,21 @@
+/*
+Problem Description
+Task. To force the given implementation of the quick sort algorithm to efficiently process sequences with
+few unique elements, your goal is replace a 2-way partition with a 3-way partition. That is, your new
+partition procedure should partition the array into three parts: < 𝑥 part, = 𝑥 part, and > 𝑥 part.
+Input Format. The first line of the input contains an integer 𝑛. The next line contains a sequence of 𝑛
+integers 𝑎0, 𝑎1, . . . , 𝑎𝑛∖1.
+Constraints. 1 ≤ 𝑛 ≤ 105; 1 ≤ 𝑎𝑖 ≤ 109 for all 0 ≤ 𝑖 < 𝑛.
+
+Output Format. Output this sequence sorted in non-decreasing order.
+
+Sample 1.
+Input:
+5
+2 3 9 2 2
+Output:
+2 2 2 3 9
+*/
 import java.io.*;
 import java.util.*;
 
