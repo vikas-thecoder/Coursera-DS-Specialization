7a6fe9650ba502fab90d2df41df1394cf3faf43b
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-28 19:52:06 +0530

    Update ChangeDP.java

--- Week 5/ChangeDP.java
+++ Week 5/ChangeDP.java
@@ -1,3 +1,23 @@
+/*
+Problem Description
+Input Format. Integer money.
+Output Format. The minimum number of coins with denominations 1, 3, 4 that changes money.
+Constraints. 1 ≤ money ≤ 103.
+
+Sample 1.
+Input:
+2
+Output:
+2
+2 = 1 + 1.
+
+Sample 2.
+Input:
+34
+Output:
+9
+34 = 3 + 3 + 4 + 4 + 4 + 4 + 4 + 4 + 4.
+*/
 import java.util.Scanner;
 
 public class ChangeDP {
