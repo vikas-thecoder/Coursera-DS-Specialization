0460c301e0a28c8af6d819a5f3ae77ce1f016a0b
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-29 19:32:23 +0530

    Update EditDistance.java

--- Week 5/EditDistance.java
+++ Week 5/EditDistance.java
@@ -1,3 +1,24 @@
+/*
+Problem Description
+Task. The goal of this problem is to implement the algorithm for computing the edit distance between two strings.
+Input Format. Each of the two lines of the input contains a string consisting of lower case latin letters.
+Constraints. The length of both strings is at least 1 and at most 100.
+Output Format. Output the edit distance between the given two strings.
+
+Sample 1.
+Input:
+ab
+ab
+Output:
+0
+
+Sample 2.
+Input:
+short
+ports
+Output:
+3
+*/
 import java.util.*;
 
 class EditDistance {
