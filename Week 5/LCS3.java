8ffa19e9fc9780f8be3f54787b77ca45b53220ba
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-08-29 19:34:35 +0530

    Update LCS3.java

--- Week 5/LCS3.java
+++ Week 5/LCS3.java
@@ -1,3 +1,37 @@
+/*
+Problem Description
+Task. Given three sequences 𝐴 = (𝑎1, 𝑎2, . . . , 𝑎𝑛), 𝐵 = (𝑏1, 𝑏2, . . . , 𝑏𝑚), and 𝐶 = (𝑐1, 𝑐2, . . . , 𝑐𝑙), find the
+length of their longest common subsequence, i.e., the largest non-negative integer 𝑝 such that there
+exist indices 1 ≤ 𝑖1 < 𝑖2 < · · · < 𝑖𝑝 ≤ 𝑛, 1 ≤ 𝑗1 < 𝑗2 < · · · < 𝑗𝑝 ≤ 𝑚, 1 ≤ 𝑘1 < 𝑘2 < · · · < 𝑘𝑝 ≤ 𝑙 such
+that 𝑎𝑖1 = 𝑏𝑗1 = 𝑐𝑘1, . . . , 𝑎𝑖𝑝 = 𝑏𝑗𝑝 = 𝑐𝑘𝑝
+Input Format. First line: 𝑛. Second line: 𝑎1, 𝑎2, . . . , 𝑎𝑛. Third line: 𝑚. Fourth line: 𝑏1, 𝑏2, . . . , 𝑏𝑚. Fifth line:𝑙. Sixth line: 𝑐1, 𝑐2, . . . , 𝑐𝑙.
+Constraints. 1 ≤ 𝑛, 𝑚, 𝑙 ≤ 100; 109 < 𝑎𝑖, 𝑏𝑖, 𝑐𝑖 < 109.
+Output Format. Output 𝑝.
+
+Sample 1.
+Input:
+3
+1 2 3
+3
+2 1 3
+3
+1 3 5
+Output:
+2
+A common subsequence of length 2 is (1, 3).
+
+Sample 2.
+Input:
+5
+8 3 2 1 7
+7
+8 2 1 3 8 10 7
+6
+6 8 3 1 4 7
+Output:
+3
+One common subsequence of length 3 in this case is (8, 3, 7). Another one is (8, 1, 7).
+*/
 import java.util.*;
 
 public class LCS3 {
