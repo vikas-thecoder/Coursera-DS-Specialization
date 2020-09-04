3fed317e8246dc31a858f35dfee1b276363192be
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-09-04 22:56:55 +0530

    Update Knapsack.java

--- Week 6/Knapsack.java
+++ Week 6/Knapsack.java
@@ -1,3 +1,21 @@
+/*
+Problem Description
+
+Task. Given 𝑛 gold bars, find the maximum weight of gold that fits into a bag of capacity 𝑊.
+Input Format. The first line of the input contains the capacity 𝑊 of a knapsack and the number 𝑛 of bars
+of gold. The next line contains 𝑛 integers 𝑤0, 𝑤1, . . . , 𝑤𝑛-1 defining the weights of the bars of gold.
+Constraints. 1 ≤ 𝑊 ≤ 104; 1 ≤ 𝑛 ≤ 300; 0 ≤ 𝑤0, . . . , 𝑤𝑛-1 ≤ 105.
+
+Output Format. Output the maximum weight of gold that fits into a knapsack of capacity 𝑊.
+
+Sample 1.
+Input:
+10 3
+1 4 8
+Output:
+9
+Here, the sum of the weights of the first and the last bar is equal to 9.
+*/
 import java.util.*;
 
 public class Knapsack {
