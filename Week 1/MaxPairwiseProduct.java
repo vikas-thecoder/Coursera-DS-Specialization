8e3bb5882d6e23d7a7a19d2d458da8a699f624c9
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-09-05 22:09:39 +0530

    Update MaxPairwiseProduct.java

--- Week 1/MaxPairwiseProduct.java
+++ Week 1/MaxPairwiseProduct.java
@@ -14,13 +14,14 @@ Note that i and j should be different, though it may be the case that ai = aj.
 Input format. The first line contains an integer n. The next line contains n non-negative integers a1 ,...,an (separated by spaces).
 Output format. The maximum pairwise product.
 Constraints. 2 ≤ n ≤ 2 · 105 ; 0 ≤ a1 ,...,an ≤ 2 · 105.
+
 Sample 1.
 Input:
 3
 1 2 3
 Output:
 6
-10
+
 Sample 2.
 Input:
 10
