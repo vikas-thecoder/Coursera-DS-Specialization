b6ba5e1d8366e07006ff60cb9917139ebd71b9f9
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-09-11 20:00:18 +0530

    Update PlacingParentheses.java

--- Week 6/PlacingParentheses.java
+++ Week 6/PlacingParentheses.java
@@ -1,3 +1,27 @@
+/*
+Problem Description
+Task. Find the maximum value of an arithmetic expression by specifying the order of applying its arithmetic
+operations using additional parentheses.
+Input Format. The only line of the input contains a string 𝑠 of length 2𝑛 + 1 for some 𝑛, with symbols
+𝑠0, 𝑠1, . . . , 𝑠2𝑛. Each symbol at an even position of 𝑠 is a digit (that is, an integer from 0 to 9) while
+each symbol at an odd position is one of three operations from {+,-,*}.
+Constraints. 0 ≤ 𝑛 ≤ 14 (hence the string contains at most 29 symbols).
+Output Format. Output the maximum possible value of the given arithmetic expression among different
+orders of applying arithmetic operations.
+
+Sample 1.
+Input:
+1+5
+Output:
+6
+
+Sample 2.
+Input:
+5-8+7*4-8+9
+Output:
+200
+200 = (5 ▽ ((8 + 7) × (4 △ (8 + 9))))
+*/
 import java.util.Scanner;
 
 public class PlacingParentheses {
