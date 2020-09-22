d049fa8ea20b084d2889e2ea90e7139bb6e8b5ce
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2020-09-22 23:58:44 +0530

    Update MajorityElement.java

--- Week 4/MajorityElement.java
+++ Week 4/MajorityElement.java
@@ -1,10 +1,8 @@
 /*
 Problem Description
 Task. The goal in this code problem is to check whether an input sequence contains a majority element.
-Input Format. The first line contains an integer 𝑛, the next one contains a sequence of 𝑛 non-negative
-integers 𝑎0, 𝑎1, . . . , 𝑎𝑛 1.
+Input Format. The first line contains an integer 𝑛, the next one contains a sequence of 𝑛 non-negative integers 𝑎0, 𝑎1, . . . , 𝑎𝑛 1.
 Constraints. 1 ≤ 𝑛 ≤ 105; 0 ≤ 𝑎𝑖 ≤ 109 for all 0 ≤ 𝑖 < 𝑛.
-
 Output Format. Output 1 if the sequence contains an element that appears strictly more than 𝑛/2 times, and 0 otherwise.
 Sample 1.
 Input:
