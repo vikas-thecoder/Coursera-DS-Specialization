d58b9cefff88d8eef935defb1d3a362f8f8acc03
vikas-thecoder <51703257+vikas-thecoder@users.noreply.github.com>
2024-05-31 22:15:18 +0530

    Update APlusB.java

--- Week 1/APlusB.java
+++ Week 1/APlusB.java
@@ -1,6 +1,5 @@
 /*
-
-                                                       Sum of Two Digits Problem
+                                                     Sum of Two Digits Problem
 Compute the sum of two single digit numbers.
 Input: Two single digit numbers.
 Output: The sum of these numbers.
